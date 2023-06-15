675c2d2f760a55ce3f0e26ed7dfd9b646c41dae8
AlbertoJTD <albertojtd@outlook.com>
2023-06-15 11:44:41 -0600

    use service inside another service

--- src/app/personas.service.ts
+++ src/app/personas.service.ts
@@ -1,12 +1,18 @@
+import { Injectable } from "@angular/core";
+import { LogginService } from "./LogginService.service";
 import { Persona } from "./persona.model";
 
+@Injectable() // Indicar que se usara un servicio dentro del servicio 'PersonasService' se usara el servicio 'LogginService'
 export class PersonasService {
   personas: Persona[] = [
     new Persona('John', 'Wick'),
     new Persona('Jonny', 'Cage')
   ];
 
+  constructor(private logginService: LogginService) { }
+
   agregarPersona(persona: Persona): void {
+    this.logginService.enviarMensajeAConsola("Agregando persona: " + persona.nombre);
     this.personas.push(persona);
   }
 }
