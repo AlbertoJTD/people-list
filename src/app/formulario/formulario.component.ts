f2e3c765d2941917b0d1703669e3538c3bd11dbc
AlbertoJTD <albertojtd@outlook.com>
2023-06-15 10:49:37 -0600

    Add LogginService

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,10 +1,12 @@
 import { Component, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { Persona } from '../persona.model';
+import { LogginService } from '../LogginService.service';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
+  styleUrls: ['./formulario.component.css'],
+  providers: [LogginService]
 })
 export class FormularioComponent {
   @Output() personaCreada = new EventEmitter<Persona>();
@@ -14,9 +16,13 @@ export class FormularioComponent {
 
   @ViewChild('nombreRef') nombre: ElementRef;
   @ViewChild('apellidoRef') apellido: ElementRef;
+  
+  // Injection of services
+  constructor(private logginService: LogginService) { }
 
   addPerson(): void {
     let newPerson = new Persona(this.nombre.nativeElement.value, this.apellido.nativeElement.value);
     this.personaCreada.emit(newPerson);
+    this.logginService.enviarMensajeAConsola('Hello from Formulario.component')
   }
 }
